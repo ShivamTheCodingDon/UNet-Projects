d31c2788bef058fd4d1a534d467956f89a61c269
Shivam Tiwari <shivamtiwari1742001@gmail.com>
2025-07-10 13:52:03 +0530

    Unet Project

---
+++ Automate Cell Segmentaion/Scripts/utils.py
@@ -0,0 +1,36 @@
+"""
+utils.py
+
+This script includes utility function for the Cell-Segmentation-UNet project. 
+These function provides general-purpose utilities that can be used in various parts of the project, 
+such as data visualization.
+
+Function:
+- display_sample_grid: Displays the random samples from the dataset, including the original images and their masks.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+def display_sample_grid(X_train, Y_train, num_samples=3):
+    """Display a grid of microscopy images and their associated masks."""
+    fig, axs = plt.subplots(2, num_samples, figsize=(12, 8))  
+
+    for i in range(num_samples):
+        idx = random.randint(0, len(X_train) - 1)
+        
+        # Display original image
+        axs[0, i].imshow(X_train[idx])
+        axs[0, i].set_title(f'Original Image {i+1}')
+        axs[0, i].axis('off')  # Turn off axis
+
+        # Display corresponding mask
+        axs[1, i].imshow(np.squeeze(Y_train[idx]))
+        axs[1, i].set_title(f'Mask Image {i+1}')
+        axs[1, i].axis('off')  # Turn off axis
+
+    plt.tight_layout()
+    plt.show()
+
+
